65fcfb83684906216ca93bf1a7308b3944eb3a6e
Denis Ryapolov <denisryapolov@Noutbuk-Denis.local>
2024-05-15 17:34:55 +0300

    task2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,9 @@ import { Command } from 'commander';
 const program = new Command();
 program
  .name('gendiff')
+ .argument("<filepath1> <filepath2>")
  .version('0.0.1', '-V, --version', 'output the version number')
+ .option('-f, --format [type]', 'output format')
  .description('Compares two configuration files and shows a difference.')
  
  program.parse();
