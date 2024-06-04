52ac61954a68985457e31278e54b7b1fa8011483
Denis <ryapolov.den@mail.ru>
2024-06-04 13:45:25 +0300

    proverka

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,14 +7,14 @@ import { pathFile } from '../src/index.js';
 const program = new Command();
 
 program
- .name('gendiff')
- .arguments('<filepath1> <filepath2>')
- .version('0.0.1', '-V, --version', 'output the version number')
- .description('Compares two configuration files and shows a difference.')
- .option('-f, --format [type]', 'output format') 
- .action((one, two) => {
-   console.log(pathFile(one, two));
-    //genDiff(one, two)
- });
+  .name('gendiff')
+  .arguments('<filepath1> <filepath2>')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .action((one, two) => {
+    console.log(pathFile(one, two));
+    // genDiff(one, two)
+  });
 
- program.parse();
+program.parse();
--- src/index.js
+++ src/index.js
@@ -6,31 +6,31 @@ import * as path from 'node:path';
 import process from 'node:process';
 
 function readFile(filepath) {
-    const pathF = path.resolve(process.cwd(), filepath);
-    return JSON.parse(fs.readFileSync(pathF))
+  const pathF = path.resolve(process.cwd(), filepath);
+  return JSON.parse(fs.readFileSync(pathF));
 }
 
 export function pathFile(filepath1, filepath2) {
-    const one = readFile(filepath1);
-    const two = readFile(filepath2);
-    //console.log(one, two); // return [file1, file2]
+  const one = readFile(filepath1);
+  const two = readFile(filepath2);
+  // console.log(one, two); // return [file1, file2]
 
-    const keys = _.union(_.keys(one), _.keys(two)).sort();
-    const diffObj = keys.map((key) => {
-        if (_.has(one, key) && !_.has(two, key)) {
-            return `  - ${key}: ${one[key]}`;
-        }
+  const keys = _.union(_.keys(one), _.keys(two)).sort();
+  const diffObj = keys.map((key) => {
+    if (_.has(one, key) && !_.has(two, key)) {
+      return `  - ${key}: ${one[key]}`;
+    }
 
-        if (_.has(two, key) && !_.has(one, key)) {
-            return`  + ${key}: ${two[key]}`;
-        }
+    if (_.has(two, key) && !_.has(one, key)) {
+      return `  + ${key}: ${two[key]}`;
+    }
 
-        if (one[key] !== two[key]) {
-            return `  - ${key}: ${one[key]}\n  + ${key}: ${two[key]}`
-        }
+    if (one[key] !== two[key]) {
+      return `  - ${key}: ${one[key]}\n  + ${key}: ${two[key]}`;
+    }
 
-        return `    ${key}: ${one[key]}`;
-    })
+    return `    ${key}: ${one[key]}`;
+  });
 
-    return `{\n${diffObj.join('\n')}\n}`;
+  return `{\n${diffObj.join('\n')}\n}`;
 }
