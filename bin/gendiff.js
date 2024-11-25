d922269b7736d3755f19fec0c941b415c98be398
Denis Ryapolov <denisryapolov@Noutbuk-Denis.local>
2024-11-25 11:13:25 +0300

    ttt

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,19 +1,18 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import { pathFile } from '../src/index.js';
+import pathFile from '../src/index.js';
  
 const program = new Command();
 
 program
-  .name('gendiff')
-  .arguments('<filepath1> <filepath2>')
-  .version('0.0.1', '-V, --version', 'output the version number')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-v, --version', 'output the version number')
   .option('-f, --format [type]', 'output format')
-  .action((one, two) => {
-    console.log(pathFile(one, two));
-    // genDiff(one, two)
+  .action((filepath1, filepath2, options) => {
+    console.log(pathFile(filepath1, filepath2, options.format));
   });
 
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,36 +1,14 @@
-#!/usr/bin/env node
+import { parseData } from './parsers.js';
+import format from './formatters/slindex.js';
 
-import fs from 'fs';
-import _ from 'lodash';
-import * as path from 'node:path';
-import process from 'node:process';
+const pathFile = (filepath1, filepath2, formatter) => {
+  const data1 = parseData(filepath1);
+  const data2 = parseData(filepath2);
+  if (data1 === null || data2 === null) {
+    console.log('wrong extension of files');
+    return false;
+  }
+  return format(data1, data2, formatter);
+};
 
-function readFile(filepath) {
-  const pathF = path.resolve(process.cwd(), filepath);
-  return JSON.parse(fs.readFileSync(pathF));
-}
-
-export function pathFile(filepath1, filepath2) {
-  const one = readFile(filepath1);
-  const two = readFile(filepath2);
-  // console.log(one, two); // return [file1, file2]
-
-  const keys = _.union(_.keys(one), _.keys(two)).sort();
-  const diffObj = keys.map((key) => {
-    if (_.has(one, key) && !_.has(two, key)) {
-      return `  - ${key}: ${one[key]}`;
-    }
-
-    if (_.has(two, key) && !_.has(one, key)) {
-      return `  + ${key}: ${two[key]}`;
-    }
-
-    if (one[key] !== two[key]) {
-      return `  - ${key}: ${one[key]}\n  + ${key}: ${two[key]}`;
-    }
-
-    return `    ${key}: ${one[key]}`;
-  });
-
-  return `{\n${diffObj.join('\n')}\n}`;
-}
+export default pathFile;
